de44ffe9e921b389d49705c6bfeef5193d714499
matt <linghuajiang@college.harvard.edu>
2019-01-20 16:51:39 -0500

    finished winner check

--- app.py
+++ app.py
@@ -15,17 +15,44 @@ def index():
     if "board" not in session:
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
+        session["winner"] = None
 
-    return render_template("game.html", game=session["board"], turn=session["turn"])
+    return render_template("game.html", game=session["board"], turn=session["turn"], winner=session["winner"])
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
-    game = session["board"]
+    #mark board
+    session["board"][row][col] = session["turn"]
+  
+    #current states
+    board = session["board"]
     turn = session["turn"]
-    game[row][col] = session["turn"]
+
+    #check for winner
+    def iswon():
+        #check rows
+        for row in board:
+            if turn == row[0] == row[1] == row[2]:
+                return True
+        #check columns
+        for col in range(3):
+            if turn == board[0][col] == board[1][col] == board[2][col]:
+                return True
+        #check diagonals 
+        if turn == board[0][0] == board[1][1] == board[2][2] or turn == board[0][2] == board[1][1] == board[2][0]:
+            return True
+        #if no winner
+        return False 
+
+    #display game over 
+    if iswon():
+        session["winner"] = turn
+        return redirect(url_for("index"))
+
+    #change turns
     if session["turn"] == "X":
         session["turn"] = "O"
     else:
         session["turn"] = "X"
-    
+            
     return redirect(url_for("index"))
