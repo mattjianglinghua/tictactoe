12ae8921a62d31ffec9530ccad739becef600089
Matt Jiang <linghuajiang@college.harvard.edu>
2019-02-12 20:51:53 -0500

    test

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#test
 def switch():
     """Switches whose turn it is."""
     if session["turn"] == "X":
